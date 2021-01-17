cfa8c06699f70cd5cef03a2b97c7c697daf33442
mrAzay <furoxaz@gmail.com>
2021-01-17 02:05:42 +0200

    token

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,15 +1,26 @@
 import firebase from 'firebase/app'
 
 export default {
+  state: {
+    token: ''
+  },
+  mutations: {
+    saveToken (state, token) {
+      state.token = token
+    }
+  },
   actions: {
     async login ({ dispatch, commit }, { email, password }) {
       try {
         await firebase.auth().signInWithEmailAndPassword(email, password)
-          .then((s) => {
-            console.log(s)
+          .then((resp) => {
+            console.log(resp)
+          })
+        await firebase.auth().currentUser.getIdToken(true)
+          .then(function (idToken) {
+            commit('saveToken', idToken)
           })
       } catch (e) {
-
       }
     }
   }
