887b17c551504e9de76b824333cb05f0c5322a64
mrAzay <furoxaz@gmail.com>
2021-01-19 19:03:49 +0200

    PIZDA 3

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,27 +1,66 @@
 <template>
-  <div class="container grid">
-    <div class="column">
-      <ProfileBlock></ProfileBlock>
-      <Paraments></Paraments>
-    </div>
-    <div class="column">
-      <Balance></Balance>
-      <History></History>
-    </div>
-    <div class="column">
-      <Aboniment></Aboniment>
-    </div>
+  <div class="container grid-container">
+    <ProfileBlock></ProfileBlock>
+    <Paraments class="paraments"></Paraments>
+    <Balance class="balance"></Balance>
+    <History class="history"></History>
+    <Aboniment class="aboniment"></Aboniment>
   </div>
 </template>
 
 <style scoped lang="scss">
-.grid {
-  margin: -20px auto;
-}
-.column {
-  max-width: 352px;
-  width: 100%;
-}
+  .grid-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: 0px 0px;
+    grid-template-areas:
+    "profile balance aboniment"
+    "profile history aboniment"
+    "paraments history aboniment";
+  }
+
+  .profile {
+    grid-area: profile;
+  }
+
+  .paraments {
+    grid-area: paraments;
+  }
+
+  .balance {
+    grid-area: balance;
+  }
+
+  .history {
+    grid-area: history;
+  }
+
+  .aboniment {
+    grid-area: aboniment;
+  }
+
+  .column {
+    max-width: 352px;
+    width: 100%;
+  }
+  @media (max-width: 1220px) {
+    .grid-container {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 191px 264px auto 240px;
+      gap: 30px 20px;
+      grid-template-areas:
+    "profile paraments"
+    "profile balance"
+    "aboniment aboniment"
+    "history history";
+    }
+    .block{
+      max-width: none;
+      margin: 0;
+    }
+  }
 </style>
 
 <script>
@@ -30,7 +69,7 @@ import Paraments from '@/components/Paraments'
 import Balance from '../components/Balance'
 import History from '../components/Historu'
 import Aboniment from '../components/Aboniment'
-import {mapActions} from 'vuex'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'Profile',
@@ -44,7 +83,7 @@ export default {
   methods: {
     ...mapActions(['GET_USER_INFO'])
   },
-  mounted() {
+  mounted () {
     this.GET_USER_INFO()
   }
 }
